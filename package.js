db507a251b0692bf987933e60f5b09027fe1691a
LB Johnston <mail@lb.ee>
2016-04-30 12:04:05 +1000

    Clean up package.js for new linting

--- package.js
+++ package.js
@@ -1,67 +1,64 @@
 const packageName = 'lbee:moment-helpers';
 const packageVersion = '1.3.3';
-const meteorVersionsFrom = '1.3';
-const momentVersion = '2.13.0';
-
-// console.log( `momentVersion: ${momentVersion}` );
+const meteorVersion = '1.3';
+const momentVersion = 'momentjs:moment@2.13.1';
 
 Package.describe({
   name: packageName,
   version: packageVersion,
   summary: 'Date format helpers from Moment.js',
   git: 'https://github.com/lb-/moment-helpers',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse( function ( api ) {
-
+const onUse = function onUse(api) {
   // meteor version
-  api.versionsFrom( meteorVersionsFrom );
+  api.versionsFrom(meteorVersion);
 
   // main package requirements
-  // api.use( `momentjs:moment@${momentVersion}` );
-  api.use( 'momentjs:moment@' + momentVersion );
-  api.use( 'ecmascript' );
-  api.use( 'reactive-var', 'client' );
-  api.use( 'templating', 'client' );
-  api.imply( 'underscore' );
-  api.imply( 'check' );
+  api.use(momentVersion);
+  api.use('ecmascript');
+  api.use('reactive-var', 'client');
+  api.use('templating', 'client');
+  api.imply('underscore');
+  api.imply('check');
 
   // main files
-  api.addFiles( 'common.js' );
-  api.addFiles( 'client.js', 'client' );
+  api.addFiles('common.js');
+  api.addFiles('client.js', 'client');
 
   // export mo
-  api.export( 'mo' );
+  api.export('mo');
+};
 
-});
-
-Package.onTest( function ( api ) {
-
+const onTest = function onTest(api) {
   // Meteor version
-  api.versionsFrom( meteorVersionsFrom );
+  api.versionsFrom(meteorVersion);
 
   // use this package
-  api.use( packageName );
+  api.use(packageName);
 
   // use packages for testing
-  api.use( 'tinytest' );
+  api.use('tinytest');
 
   // use packages that are needed for this package
-  api.use( 'ecmascript' );
-  api.use( 'templating', 'client' );
-  api.use( 'reactive-var', 'client' );
+  api.use('ecmascript');
+  api.use('templating', 'client');
+  api.use('reactive-var', 'client');
 
   // use Moment package
-  api.use( 'momentjs:moment@' + momentVersion );
+  api.use(momentVersion);
 
   // add main files
-  api.addFiles( 'common.js' );
-  api.addFiles( 'client.js', 'client' );
+  api.addFiles('common.js');
+  api.addFiles('client.js', 'client');
 
   // add test files
-  api.addFiles( 'tests/templates.html', 'client' );
-  api.addFiles( 'tests/tests.js' );
-  api.addFiles( 'tests/locale_tests.js', 'client' );
+  api.addFiles('tests/templates.html', 'client');
+  api.addFiles('tests/tests.js');
+  api.addFiles('tests/locale_tests.js', 'client');
+};
 
-});
+
+Package.onTest(onTest);
+Package.onUse(onUse);
